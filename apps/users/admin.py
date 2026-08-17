9b6c7398dc3abeb5b389b035ce620821e19f628a
akmal.urinoff <akmalikronovich86@gmail.com>
2026-08-17 06:04:36 +0500

    fix: add telegram_user_id to UserAdmin, create migration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.html import format_html
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'full_name', 'phone', 'email', 'role', 'telegram_user_id', 'is_active')
+    list_display = ('username', 'full_name', 'phone', 'email', 'role', 'telegram_status', 'is_active')
     list_filter = ('role', 'is_active', 'is_staff')
     search_fields = ('username', 'full_name', 'email', 'phone', 'telegram_user_id')
     ordering = ('username',)
+
+    def telegram_status(self, obj):
+        if obj.telegram_user_id:
+            return format_html('<span style="color: green;">✅ {}</span>', obj.telegram_user_id)
+        return format_html('<span style="color: gray;">❌ bog\'lanmagan</span>')
+    telegram_status.short_description = 'Telegram'
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -20,9 +26,12 @@ class UserAdmin(BaseUserAdmin):
                 'email',
                 'full_name',
                 'phone',
-                'telegram_user_id',
-                'position'
+                'position',
             )
+        }),
+        ('Telegram', {
+            'fields': ('telegram_user_id',),
+            'description': 'Foydalanuvchi /start yuborganda bot ko\'rsatadigan ID ni shu yerga kiriting',
         }),
         ('Rol va holat', {
             'fields': (
@@ -31,7 +40,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_staff',
                 'is_superuser',
                 'groups',
-                'user_permissions'
+                'user_permissions',
             )
         }),
         ('Muhim malumotlar', {
@@ -49,7 +58,7 @@ class UserAdmin(BaseUserAdmin):
                 'full_name',
                 'phone',
                 'email',
-                'role'
+                'role',
             ),
         }),
     )
